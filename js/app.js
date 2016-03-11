c853dfa3ec34c5faa294b0010e653a4f4acfdb69
Tihomir <raylight75@gmail.com>
2016-03-11 15:42:57 +0200

    AngularJs-Filters

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/* App Module */
+
+var phonecatApp = angular.module('phonecatApp', [
+    'ngRoute',
+    'phonecatControllers',
+    'phonecatFilters',
+    'phonecatServices',
+    'angularUtils.directives.dirPagination'
+]);
+
+phonecatApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/phones', {
+                templateUrl: 'partials/phone-list.html',
+                controller: 'PhoneListCtrl'
+            }).
+            when('/phones/:phoneId', {
+                templateUrl: 'partials/phone-detail.html',
+                controller: 'PhoneDetailCtrl'
+            }).
+            otherwise({
+                redirectTo: '/phones'
+            });
+    }]);
---
+++ js/controllers.js
@@ -0,0 +1,44 @@
+/* Controllers */
+
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+    function ($scope, Phone) {
+        // $http.get('phones/phones.json').success(function(data) {
+        //   $scope.phones = data;
+        //   console.log($scope.phones);
+        // });
+        $scope.phones = Phone.query();
+        $scope.nameIncludes = [];
+        console.log($scope.phones);
+
+        $scope.includeName = function (name) {
+            var i = $.inArray(name, $scope.nameIncludes);
+            if (i > -1) {
+                $scope.nameIncludes.splice(i, 1);
+            } else {
+                $scope.nameIncludes.push(name);
+            }
+            console.log($scope.nameIncludes);
+        }
+
+        $scope.nameFilter = function (phones) {
+            if ($scope.nameIncludes.length > 0) {
+                if ($.inArray(phones.slug, $scope.nameIncludes) < 0)
+                    return;
+            }
+            return phones;
+        }
+        $scope.orderProp = 'age';
+    }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+    function ($scope, $routeParams, Phone) {
+        $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function (phone) {
+            $scope.mainImageUrl = phone.images[0];
+        });
+
+        $scope.setImage = function (imageUrl) {
+            $scope.mainImageUrl = imageUrl;
+        };
+    }]);
